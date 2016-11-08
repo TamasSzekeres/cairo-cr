fee5410afed50b8b939321aa383bc37d49b19afb
TamasSzekeres <szektam2@gmail.com>
2016-11-08 20:43:22 +0100

    add: pdf.cr

---
+++ src/cairo/pdf.cr
@@ -0,0 +1,48 @@
+require "./cairo"
+
+{% if CairoCr::HAS_PDF_SURFACE %}
+  module CairoCr
+    @[Link("cairo")]
+    lib Cairo
+      enum PdfVersionT
+        V_1_4,
+        V_1_5
+      end
+
+      fun pdf_surface_create = cairo_pdf_surface_create(
+        filename : UInt8*,
+        width_in_points : Float64,
+        height_in_points : Float64
+      ) : PSurfaceT
+
+      fun pdf_surface_create_for_stream = cairo_pdf_surface_create_for_stream(
+        write_func : WriteFuncT,
+        closure : Void*,
+        width_in_points : Float64,
+        height_in_points : Float64
+      ) : PSurfaceT
+
+      fun pdf_surface_restrict_to_version = cairo_pdf_surface_restrict_to_version(
+        surface : PSurfaceT,
+        version : PdfVersionT
+      ) : Void
+
+      fun pdf_get_versions = cairo_pdf_get_versions(
+        versions : PdfVersionT**,
+        num_versions : Int32*
+      ) : Void
+
+      fun pdf_version_to_string = cairo_pdf_version_to_string(
+        version : PdfVersionT
+      ) : UInt8*
+
+      fun pdf_surface_set_size = cairo_pdf_surface_set_size(
+        surface : PSurfaceT,
+        width_in_points : Float64,
+        height_in_points : Float64
+      ) : Void
+    end # lib Cairo
+  end # module CairoCr
+{% else %} # CairoCr::HAS_XLIB_SURFACE
+  puts "Cairo was not compiled with support for the pdf backend"
+{% end %} # CairoCr::HAS_XLIB_SURFACE
